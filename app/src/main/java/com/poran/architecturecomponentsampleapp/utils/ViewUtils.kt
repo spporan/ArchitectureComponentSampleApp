2636dd6d28dba0b13f9fb1b093c93c34e14b0708
ridmik <poran.cse@gmail.com>
2020-11-21 00:49:08 +0600

    created Coroutines.kt Object for creating some coroutine scope

--- app/src/main/java/com/poran/architecturecomponentsampleapp/utils/ViewUtils.kt
+++ app/src/main/java/com/poran/architecturecomponentsampleapp/utils/ViewUtils.kt
@@ -1,9 +1,17 @@
 package com.poran.architecturecomponentsampleapp.utils
 
 import android.content.Context
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 
 fun Context.toast(msg:String){
     Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
 
+}
+fun ProgressBar.show(){
+    this.visibility= View.VISIBLE
+}
+fun ProgressBar.hide(){
+    this.visibility=View.GONE
 }

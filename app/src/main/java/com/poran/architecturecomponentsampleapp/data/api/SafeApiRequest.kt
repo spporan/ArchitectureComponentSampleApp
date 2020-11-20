e81859b71cc5bdd74bc68c8999c7f81288921178
ridmik <poran.cse@gmail.com>
2020-11-21 01:31:25 +0600

    added SafeApiRequest.kt and Exceptions.kt

---
+++ app/src/main/java/com/poran/architecturecomponentsampleapp/data/api/SafeApiRequest.kt
@@ -0,0 +1,31 @@
+package com.poran.architecturecomponentsampleapp.data.api
+
+import com.poran.architecturecomponentsampleapp.utils.ApiException
+import org.json.JSONException
+import org.json.JSONObject
+import retrofit2.Response
+import java.lang.StringBuilder
+
+abstract class SafeApiRequest {
+    suspend fun<T:Any> apiRequest(call:suspend ()->Response<T>):T{
+        val  response=call.invoke()
+        if(response.isSuccessful){
+            return response.body()!!
+        }else{
+            val errorMsg=StringBuilder()
+            val error=response.errorBody().toString()
+            error.let {
+                try {
+                    errorMsg.append(JSONObject(it).getString("message"))
+                }catch (e:JSONException){}
+                finally {
+                    errorMsg.append("\n")
+                    errorMsg.append("Error code ${response.code()}")
+                }
+
+                throw ApiException(errorMsg.toString())
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/poran/architecturecomponentsampleapp/utils/Exceptions.kt
@@ -0,0 +1,6 @@
+package com.poran.architecturecomponentsampleapp.utils
+
+import java.io.IOException
+
+class ApiException( message:String):IOException(message) {
+}

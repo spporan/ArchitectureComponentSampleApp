0200326ac31c05a0ba2d21ed3c67702b85e24348
ridmik <poran.cse@gmail.com>
2020-11-21 12:33:43 +0600

    modified UserRepository.kt for adding another suspend fun for saving user data

--- app/src/main/java/com/poran/architecturecomponentsampleapp/data/repository/UserRepository.kt
+++ app/src/main/java/com/poran/architecturecomponentsampleapp/data/repository/UserRepository.kt
@@ -5,14 +5,24 @@ import androidx.lifecycle.MutableLiveData
 import com.poran.architecturecomponentsampleapp.data.api.ApiService
 import com.poran.architecturecomponentsampleapp.data.api.SafeApiRequest
 import com.poran.architecturecomponentsampleapp.data.api.respones.AuthResponse
+import com.poran.architecturecomponentsampleapp.data.db.AppDatabase
+import com.poran.architecturecomponentsampleapp.data.db.entities.User
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
 
-class UserRepository : SafeApiRequest(){
+class UserRepository (
+        private val apiService:ApiService,
+        private val appDatabase: AppDatabase
+): SafeApiRequest(){
 
   suspend  fun userLogin(email:String,password:String):AuthResponse{
       return  apiRequest { ApiService().userLogin(email,password) }
 
     }
+
+    suspend fun saveUser(user:User){
+        appDatabase.userDao().insert(user)
+    }
+    fun getUser()=appDatabase.userDao().getUSer()
 }
